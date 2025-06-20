66863b9101f949c74eed01f08f35fbb5fe907921
RafaelVirginio1805 <rafaelvirginiosilva1805@gmail.com>
2025-06-19 21:40:27 -0300

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,22 +1,39 @@
+const CACHE_NAME = "app-cache-v2";
+const URLS_TO_CACHE = [
+    "/",
+    "/static/css/style.css",
+    "/static/js/script.js",
+    "/static/icons/icon-192.png",
+    "/static/icons/icon-512.png",
+    // adicione aqui todos os arquivos que seu app precisa offline
+];
+
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open("app-cache").then((cache) => {
-            return cache.addAll([
-                "/",
-                "/static/css/style.css",
-                "/static/js/script.js",
-                "/static/icons/icon-192.png",
-                "/static/icons/icon-512.png"
-            ]);
-        })
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
     );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if (cacheName !== CACHE_NAME) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    );
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
+        }).catch(() => {
+            // opcional: retornar uma página offline personalizada
         })
     );
 });
-
